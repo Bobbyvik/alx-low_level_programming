8bb55a867b5eac823419b715bd96f120a608b300
Bobbyvik <victoriroegbu93@gmail.com>
2022-12-14 14:39:36 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,16 +2,15 @@
 
 /**
 * print_alphabet -> prints the lowercase alphabets
-*
 */
 
 void print_alphabet(void)
 {
-	int j;
+int j;
 
-	for (j = 'a'; j <= 'z'; j++)
-	{
-	    _putchar(j);
-	}
-	_putchar('\n');
+for (j = 'a'; j <= 'z'; j++)
+{
+putchar(j);
+}
+_putchar('\n');
 }
