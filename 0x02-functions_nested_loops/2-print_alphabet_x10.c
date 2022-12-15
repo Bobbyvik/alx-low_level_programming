3a986e3dd71cc78e673af48ca2d8fbdf6f61126d
Bobbyvik <victoriroegbu93@gmail.com>
2022-12-14 20:58:20 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,15 +6,15 @@
 
 void print_alphabet_x10(void)
 {
-int j;
-int x;
+	int j;
+	int x;
 
-for (x = 0; x < 10; x++)
-{
-
-	for (j = 'a'; j <= 'z'; j++)
+	for (x = 0; x < 10; x++)
 	{
-	    _putchar(j);
+		for (j = 'a'; j <= 'z'; j++)
+		{
+		_putchar(j);
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
